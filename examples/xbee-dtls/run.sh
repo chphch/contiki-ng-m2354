3676d6eb9b7284079faae57eb92d59acd89425d1
Hyunwoo Jung <chaosphch@gmail.com>
2023-12-18 18:28:10 +0900

    Improve run.sh

--- examples/xbee-dtls/run.sh
+++ examples/xbee-dtls/run.sh
@@ -1,14 +1,17 @@
+COMPORT=8
+
+taskkill.exe /FI "WINDOWTITLE eq "COM$COMPORT" - Tera Term VT"
+
 set -e
 make TARGET=m2354 BOARD=ASM2 NS=1
 
-~/spuos/dist/axtool.exe -t nu_maker -f2 build/m2354/ASM2/dtls-client.bin COM3 &
+~/spuos/dist/axtool.exe -t nu_maker -f2 build/m2354/ASM2/dtls-client.bin "COM$COMPORT" &
 sleep 1
 NuLink.exe -reset
 wait
 
 set +e
 
-(/mnt/c/Program\ Files\ \(x86\)/teraterm/ttermpro.exe /C=3 /BAUD=115200) &
+(/mnt/c/Program\ Files\ \(x86\)/teraterm/ttermpro.exe "/C=$COMPORT" /BAUD=115200) &
 sleep 1
 NuLink.exe -reset
-wait
---
+++ examples/xbee-dtls/terminal.sh
@@ -0,0 +1,3 @@
+(/mnt/c/Program\ Files\ \(x86\)/teraterm/ttermpro.exe /C=8 /BAUD=115200) &
+sleep 1
+NuLink.exe -reset
